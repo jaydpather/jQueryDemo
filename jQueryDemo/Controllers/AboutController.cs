0c47227917c7843899aa32e027b5da54ccb15b36
jaydpather <jaydpather@gmail.com>
2014-01-21 18:31:00 -0600

    initial check-in

---
+++ jQueryDemo/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryDemo.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+
+        public ActionResult MissionStatement()
+        {
+            return View();
+        }
+	}
+}
---
+++ jQueryDemo/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Household()
+        {
+            return View();
+        }
+
+        public ActionResult Clothing()
+        {
+            return View();
+        }
+	}
+}
